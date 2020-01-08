51763a053811e46043dc2e3c6d161e34bc670c76
Louis <cj654eji618@gmail.com>
2020-01-08 11:23:54 +0800

    webpack basic fix

---
+++ webpack-project/src/search.js
@@ -0,0 +1,19 @@
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import logo from "./images/logo.svg";
+import "./search.less";
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div className="search-text">
+        Search Text Hello
+        <img src={logo} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Search />, document.getElementById("root"));
